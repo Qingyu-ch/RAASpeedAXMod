4bfad3db87ce78d96e1906744f0449245f616afc
Qingyu-ch <yxcne@qq.com>
2026-07-15 11:17:10 +0800

    Delete customize.sh

--- customize.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-# Restore Animations 1.0x — AxManager plugin
-# 把开发者选项三个动画缩放还原为 1.0x
-
-echo "- Installing Restore Animations 1.0x"
-echo "- Reset window / transition / animator scale to 1.0x"
-echo "- Version: v1.0"
-echo "- 刷入后点击左下角执行！"
